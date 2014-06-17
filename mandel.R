4718980b4dd43a49576b1623d01fda143d43667f
Lech Gudalewicz <lechg@tvarkoj.com>
2014-06-16 17:16:15 -0700

    Final tweaks

--- mandel.R
+++ mandel.R
@@ -1,3 +1,11 @@
+#
+#
+#
+# First, naive implementation in pure R
+#
+#
+#
+
 mandel.r <- function(w = 320, h = 200, start = complex(real = -1.8, imaginary = -1.2), end = complex(real = 0.6, imaginary = 1.2)) {
     
     iter <- function(c) {
@@ -26,23 +34,131 @@ mandel.r <- function(w = 320, h = 200, start = complex(real = -1.8, imaginary = 
     m
 }
 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# C-based implementation
+#
+#
+#
+
+
 mandel.cpu <- function(w = 320, h = 200, start = complex(real = -1.8, imaginary = -1.2), end = complex(real = 0.6, imaginary = 1.2)) {
     res <- .C("mandel_cpu", as.integer(w), as.integer(h), as.double(Re(start)), as.double(Re(end)), as.double(Im(start)), as.double(Im(end)), result = double(w * h))
     matrix(res[["result"]], nrow = h, ncol = w)
 }
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# CUDA-based implementation
+#
+#
+#
 
 mandel.gpu <- function(w = 320, h = 200, start = complex(real = -1.8, imaginary = -1.2), end = complex(real = 0.6, imaginary = 1.2)) {
     res <- .C("mandel_gpu", as.integer(w), as.integer(h), as.double(Re(start)), as.double(Re(end)), as.double(Im(start)), as.double(Im(end)), result = double(w * h))
     matrix(res[["result"]], nrow = h, ncol = w)
 }
 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# some helpers
+# 
+#
+#
+#
+
 capture.stat <- function() {
     df <- data.frame()
     # warmup
     mandel.gpu()
     mandel.cpu()
-    for (i in seq(800, 8000, 800)) {
+    for (i in seq(800, 7200, 800)) {
+        print(sprintf("Processing CPU code for %dx%d", i, i));
         cpu <- system.time(mandel.cpu(i, i))[['elapsed']]
+        print(sprintf("Processing GPU code for %dx%d", i, i));
         gpu <- system.time(mandel.gpu(i, i))[['elapsed']]
         df1 <- data.frame(size=i, cpu=cpu, gpu=gpu)
         df <- rbind(df, df1)
